f848cb891db777035ef4cb3a2ac2ccce90790503
Andriy Hedzyk <andriy.hedzyk@gmail.com>
2022-08-06 15:00:04 -0400

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,6 +58,22 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== todoId),
     }));
   };
+
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts)
+    if (parsedContacts) {
+    this.setState({ contacts: parsedContacts })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+        if (this.state.contacts !== prevState.state) { // якщо теперішній масив не такий самий як попередній
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    } 
+  }
+  
   render() {
     const { contacts, filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
